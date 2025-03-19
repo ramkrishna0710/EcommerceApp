d55d31065fb9ee59deabe68f9dc13fee47be1742
ramkrishna0710 <113228237+ramkrishna0710@users.noreply.github.com>
2025-03-19 22:58:30 +0530

    first commit

--- app/index.tsx
+++ app/index.tsx
@@ -1,24 +1,82 @@
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { ImageBackground, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
-import { Link } from "expo-router";
+import { Link, Stack } from "expo-router";
+import { LinearGradient } from "expo-linear-gradient";
+import { Colors } from "@/constants/Colors";
+import { Ionicons } from "@expo/vector-icons";
+import Google from '@/assets/images/google-logo.svg'
+import Animated, { FadeInDown, FadeInRight } from "react-native-reanimated";
 
 type Props = {};
 
 const WelcomeScreen = (props: Props) => {
   return (
-    <View style={styles.container}>
-      <Text>Welcome Screen</Text>
-      <Link href={"/signin"} asChild>
-        <TouchableOpacity>
-          <Text>Go to SignIn Screen</Text>
-        </TouchableOpacity>
-      </Link>
-      <Link href={"/signup"} asChild>
-        <TouchableOpacity>
-          <Text>Go to SignUp Screen</Text>
-        </TouchableOpacity>
-      </Link>
-    </View>
+    <>
+      <Stack.Screen options={{ headerShown: false }} />
+      <ImageBackground
+        source={require('@/assets/images/ecommerce-splash.jpg')}
+        style={{ flex: 1 }}
+        resizeMode="cover"
+      >
+        <View style={styles.container}>
+          <LinearGradient
+            colors={['transparent', "rgba(255,255,255,0.9)", "rgba(255,255,255,1)"]}
+            style={styles.background}
+          >
+            <View style={styles.wrapper}>
+
+              {/* ShopX text */}
+              <Animated.Text style={styles.title} entering={FadeInRight.delay(300).duration(300)}>ShopX</Animated.Text>
+              <Animated.Text style={styles.description} entering={FadeInRight.delay(500).duration(300)}>One Stop Solution for All Your Needs.</Animated.Text>
+
+              {/* Continue with Email button */}
+              <View style={styles.socialLoginWrapper}>
+                <Animated.View entering={FadeInDown.delay(300).duration(500).springify()}>
+                  <Link href={"/signup"} asChild>
+                    <TouchableOpacity style={styles.button}>
+                      <Ionicons
+                        name="mail-outline" size={20} color={Colors.black}
+                      />
+                      <Text style={styles.btnText}>Continue with Email</Text>
+                    </TouchableOpacity>
+                  </Link>
+                </Animated.View>
+              </View>
+              {/* Continue with google button */}
+              <View style={styles.socialLoginWrapper}>
+                <Animated.View entering={FadeInDown.delay(700).duration(500).springify()}>
+                  <TouchableOpacity style={styles.button}>
+                    <Google
+                      width={20} height={20}
+                    />
+                    <Text style={styles.btnText}>Continue with Google</Text>
+                  </TouchableOpacity>
+                </Animated.View>
+              </View>
+              {/* Continue with apple button */}
+              <View style={styles.socialLoginWrapper}>
+                <Animated.View entering={FadeInDown.delay(1100).duration(500).springify()}>
+                  <TouchableOpacity style={styles.button}>
+                    <Ionicons
+                      name="logo-apple" size={20} color={Colors.black}
+                    />
+                    <Text style={styles.btnText}>Continue with Apple</Text>
+                  </TouchableOpacity>
+                </Animated.View>
+              </View>
+              <View style={styles.loginContainer}>
+                <Text style={styles.loginTxt}>Already Have an account?{" "}</Text>
+                <Link href={"/signin"} asChild>
+                  <TouchableOpacity>
+                    <Text style={styles.loginTxtSpan}>SignIn</Text>
+                  </TouchableOpacity>
+                </Link>
+              </View>
+            </View>
+          </LinearGradient>
+        </View>
+      </ImageBackground>
+    </>
   );
 };
 
@@ -30,4 +88,65 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
+  background: {
+    flex: 1,
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    justifyContent: 'flex-end'
+  },
+  wrapper: {
+    paddingBottom: 50,
+    paddingHorizontal: 20,
+    alignItems: 'center'
+  },
+  title: {
+    fontSize: 22,
+    color: Colors.primary,
+    fontWeight: '700',
+    letterSpacing: 2.4,
+    marginBottom: 5,
+  },
+  description: {
+    fontSize: 12,
+    color: Colors.gray,
+    letterSpacing: 1.2,
+    lineHeight: 30,
+    marginBottom: 20,
+  },
+  socialLoginWrapper: {
+    alignSelf: 'stretch'
+  },
+  button: {
+    flexDirection: 'row',
+    padding: 10,
+    borderColor: Colors.gray,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderRadius: 25,
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: 5,
+    marginBottom: 15
+  },
+  btnText: {
+    fontSize: 14,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  loginContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 30
+  },
+  loginTxt: {
+    fontSize: 14,
+    color: Colors.black,
+  },
+  loginTxtSpan: {
+    color: Colors.primary,
+    fontWeight: '600',
+  }
 });
--- app/signup.tsx
+++ app/signup.tsx
@@ -1,13 +1,55 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import React from 'react'
+import { Link, router, Stack } from 'expo-router'
+import { Colors } from '@/constants/Colors'
+import { Ionicons } from '@expo/vector-icons'
+import InputField from '@/components/InputField'
 
 type Props = {}
 
 const SignUpScreen = (props: Props) => {
   return (
-    <View style={styles.container}>
-      <Text>SignUp Screen</Text>
-    </View>
+    <>
+      <Stack.Screen options={{
+        headerTitle: 'Sign Up', headerLeft: () => (
+          <TouchableOpacity onPress={() => router.back()}>
+            <Ionicons name="close" size={24} color={Colors.black} />
+          </TouchableOpacity>
+        )
+      }} />
+      <View style={styles.container}>
+        <Text style={styles.title}>Create an Account</Text>
+        <InputField
+          placeholder="Enter email address"
+          placeholderTextColor={Colors.gray}
+          autoCapitalize='none'
+          keyboardType='email-address'
+        />
+        <InputField
+          placeholder="Password"
+          placeholderTextColor={Colors.gray}
+          secureTextEntry={true}
+        />
+        <InputField
+          placeholder="Confrim Password"
+          placeholderTextColor={Colors.gray}
+          secureTextEntry={true}
+        />
+        <TouchableOpacity style={styles.button} onPress={() => router.push('/')}>
+          <Text style={styles.buttonText}>Create an account</Text>
+        </TouchableOpacity>
+
+        <View style={styles.loginContainer}>
+          <Text style={styles.loginTxt}>Already Have an account?{" "}</Text>
+          <Link href={"/signin"} asChild>
+            <TouchableOpacity>
+              <Text style={styles.loginTxtSpan}>SignIn</Text>
+            </TouchableOpacity>
+          </Link>
+        </View>
+        <View style={styles.divider}/>
+      </View>
+    </>
   )
 }
 
@@ -17,6 +59,49 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    padding: 20,
+    backgroundColor: Colors.background
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: '600',
+    letterSpacing: 1.2,
+    marginBottom: 50,
+    color: Colors.black
+  },
+  button: {
+    backgroundColor: Colors.primary,
+    paddingVertical: 14,
+    paddingHorizontal: 18,
+    alignSelf: 'stretch',
+    alignItems: 'center',
+    borderRadius: 5,
+    marginBottom: 20
+  },
+  buttonText: {
+    color: Colors.white,
+    fontSize: 16,
+    fontWeight: '600'
+  },
+  loginContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 30
+  },
+  loginTxt: {
+    fontSize: 14,
+    color: Colors.black,
+  },
+  loginTxtSpan: {
+    color: Colors.primary,
+    fontWeight: '600',
+  },
+  divider: {
+    borderTopColor: Colors.gray,
+    borderTopWidth: StyleSheet.hairlineWidth,
+    width: '30%',
+    marginBottom: 10,
   }
 })
